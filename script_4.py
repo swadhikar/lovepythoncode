e3f490af97747020bb4fcd537a968d9c04c9557f
swadhi <cswadhikar@gmail.com>
2019-07-08 22:13:27 +0530

    post 4

--- script_4.py
+++ script_4.py
@@ -18,3 +18,15 @@ print('\nPythonic approach using "enumerate()" function')
 
 for index, alphabet in enumerate(large_list, start=1):
     print('{}. {}'.format(index, alphabet))
+
+"""
+The enumerate(list) function takes in a list or tuple and returns
+a list of tuples with the auto manipulated index as the first element of 
+tuple and the second element with the iterated value. For example,
+
+enumerate(['a', 'b', 'c']) would return [(0, 'a'), (1, 'b'), (2, 'c')]
+
+where the index by default is 0. we need to set the start keyword argument
+equal to 1 to want indexes from 1, 2, 3 and so on. 
+
+"""
