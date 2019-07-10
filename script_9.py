a0b15b532635c7f4f416888636f273ae360ba0f6
swadhi <cswadhikar@gmail.com>
2019-07-10 22:08:15 +0530

    post 9

---
+++ script_9.py
@@ -0,0 +1,17 @@
+class StringNotAllowedException(Exception):  # inherit from Exception class
+    pass
+
+
+""" Program to calculate square root of numbers """
+unclean_list = [7, 'eight', 9, 'ten']    # A random list
+
+for num in unclean_list:
+    try:
+        if type(num) is str:
+            raise StringNotAllowedException  # raise exception intentionally
+
+        square_root = num ** 0.5
+        print('square root of "{}": {}'.format(num, square_root
+
+    except StringNotAllowedException as e:
+        print('<<<Invalid number: {}>>>'.format(num))  # handle exception
