8319dd3e79bf3c3c57f036706b32a611e0e4caa1
swadhi <cswadhikar@gmail.com>
2019-07-12 10:18:08 +0530

    post 12

--- script_12.py
+++ script_12.py
@@ -1,3 +1,9 @@
+"""
+    Multiple imports from a single module such as classes or functions
+    can be multilined similar to the way we define a tuple object. This
+    makes the import look so elegant and prevents a lengthy long ugly lines
+    of code
+"""
 from time import (
     sleep,
     ctime,
