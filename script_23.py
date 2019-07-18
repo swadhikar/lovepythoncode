9d60e0cbf6e774670359cf60f27ef02503cd6988
swadhi <cswadhikar@gmail.com>
2019-07-18 09:00:27 +0530

    post 23

---
+++ script_23.py
@@ -0,0 +1,28 @@
+class MyClass:
+    def __init__(self):
+        """Object constructor"""
+        pass
+
+    def test_method(self, arg_1, arg_2):
+        """
+            A simple test method that does nothing
+            @param   arg_1 (string)   simple argument
+            @param   arg_2 (int)      another argument
+        """
+        pass
+
+print( help(MyClass) )
+
+# class MyClass(builtins.object)
+#  |  This is a demo class that shows the use of docstrings!
+#  |
+#  |  Methods defined here:
+#  |
+#  |  __init__(self)
+#  |      Object constructor
+#  |
+#  |  test_method(self, arg_1, arg_2)
+#  |      A simple test method that does nothing
+#  |      @param   arg_1 (string)   simple argument
+#  |      @param   arg_2 (int)      another argument
+#  |
