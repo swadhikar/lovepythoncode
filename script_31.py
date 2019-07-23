a22fc9cd95b9c425dde41513a157bb22ee628ca9
Swadhikar <31162445+swadhikar@users.noreply.github.com>
2019-07-23 17:06:56 +0530

    Update script_31.py

--- script_31.py
+++ script_31.py
@@ -3,3 +3,5 @@ in_list = [1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 6, 7]
 # 1. Convert to dictionary of None values and rebuild list
 in_list = list(   {item: None for item in in_list}.keys()   )
 print( in_list )    # [1, 2, 3, 4, 5, 6, 7]
+
+""" The results are from python 3.7 execution. List ordering may vary if ran from versions < 3.7 """
